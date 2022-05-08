fc83e141ff9546ad2f92c156e932158a57018e51
sulapis <sulapis@outlook.com>
2022-05-09 05:25:14 +0800

    ,

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use embedded_time::rate::*;
 use hal::gpio::Function;
 use hal::gpio::I2C;
 use hal::gpio::{bank0::*, Output, Pin, PushPull};
-use heapless::Vec;
+// use heapless::Vec;
 use panic_halt as _;
 use rp_pico::hal;
 use rp_pico::hal::pac;
@@ -104,6 +104,8 @@ fn main() -> ! {
     let as5600 = As5600::new(i2c);
     let mut movement = MoveMent::new(as5600);
 
+    // let mut i2cs
+
     //驱动器
     let mut ena = pins.gpio22.into_push_pull_output();
     ena.set_low().unwrap();
@@ -114,24 +116,65 @@ fn main() -> ! {
     let mut pulse = Pulse::new(ena, dir, pul);
 
     let mut led = pins.led.into_push_pull_output();
-    led.set_low().unwrap();
+    led.set_high().unwrap();
     unsafe {
         pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
     };
 
     let mut n = 0;
+    // delay.delay_ms(2000);
+
+    //匀变速  50 deg/s^2   2秒
+    //匀速   100 deg/s    2秒
+    //匀变速 -50 deg/s^2  2秒
+    //匀变速 -50 deg/s^2  2秒
+    //匀速   -100 deg/s   2秒
+    //匀变速  50 deg/s^2  2秒
+    //共12秒
+
+    movement
+        .add_point(RPoint::new(0.0, 0.0, 50.0, 2.0, 2))
+        .unwrap();
+    movement
+        .add_point(RPoint::new(0.0, 100.0, 0.0, 2.0, 1))
+        .unwrap();
+    movement
+        .add_point(RPoint::new(0.0, 100.0, -50.0, 2.0, 2))
+        .unwrap();
+    movement
+        .add_point(RPoint::new(0.0, 0.0, -50.0, 2.0, 2))
+        .unwrap();
+    movement
+        .add_point(RPoint::new(0.0, -100.0, 0.0, 2.0, 1))
+        .unwrap();
+    movement
+        .add_point(RPoint::new(0.0, -100.0, 50.0, 2.0, 2))
+        .unwrap();
+
+    // movement
+    //     .add_point(RPoint::new(90.0, 1.0, 0.0, 4.0, 0))
+    //     .unwrap();
+    // movement
+    //     .add_point(RPoint::new(0.0, 1.0, 0.0, 6.0, 0))
+    //     .unwrap();
+    // movement
+    //     .add_point(RPoint::new(-180.0, 0.0, 0.0, 2.0, 0))
+    //     .unwrap();
+    movement.start(&mut pulse, &mut delay).unwrap();
+
+    delay.delay_ms(3000);
 
     loop {
         // pulse.sent_pulse(&mut delay);
-        n += 1;
-        if n >= 8288 {
-            n = 0;
-            delay.delay_ms(500);
-        }
+        // n += 1;
+        // if n >= 8288 {
+        //     n = 0;
+        //     delay.delay_ms(500);
+        // }
 
-        pulse.sent_pulse(&mut delay);
+        // pulse.sent_pulse(&mut delay);
 
-        delay.delay_us(200);
+        // delay.delay_us(200);
     }
 }
 
@@ -305,27 +348,141 @@ impl Pulse {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct RPoint {
     angle: f64,
     speed: f64,
     a: f64,
     time: f64,
+    //0: 给定运行时间 1: 匀速 2: 变速
+    mode: usize,
 }
 impl RPoint {
-    fn new(angle: f64, speed: f64, a: f64, time: f64) -> Self {
+    /// mode:
+    ///
+    /// 0: 给定运行时间| angle:目标角度 time:时间
+    ///
+    /// 1: 匀速| speed:速度 time: 持续时间
+    ///
+    /// 2: 变速| speed:初始速度 a:加速度 time:持续时间
+    fn new(angle: f64, speed: f64, a: f64, time: f64, mode: usize) -> Self {
         Self {
             angle,
             speed,
             a,
             time,
+            mode,
         }
     }
 
     fn start(&self, now_angle: f64, bit_angle: f64, pulse: &mut Pulse, delay: &mut Delay) -> i32 {
+        match self.mode {
+            0 => self.time_speed(now_angle, pulse, delay, bit_angle),
+            1 => self.uniform_speed(now_angle, pulse, delay, bit_angle),
+            2 => self.variable_speed(now_angle, pulse, delay, bit_angle),
+            _ => 0,
+        }
+    }
+
+    fn time_speed(
+        &self,
+        now_angle: f64,
+        pulse: &mut Pulse,
+        delay: &mut Delay,
+        bit_angle: f64,
+    ) -> i32 {
         let n = ((self.angle - now_angle) / bit_angle) as i32;
-        let speed = (self.time * 1000000.0 / n as f64) as u32;
-        for i in 0..n {
+
+        let num = n.abs();
+        let speed = (self.time * 1000_000.0 / num as f64) as u32;
+        if now_angle < self.angle {
+            pulse.change_dir(0);
+        } else {
+            pulse.change_dir(1);
+        }
+        for _ in 0..num {
+            pulse.sent_pulse(delay);
+            delay.delay_us(speed);
+        }
+
+        n
+    }
+    fn uniform_speed(
+        &self,
+        now_angle: f64,
+        pulse: &mut Pulse,
+        delay: &mut Delay,
+        bit_angle: f64,
+    ) -> i32 {
+        let n = (self.speed * self.time / bit_angle) as i32;
+        let num = n.abs();
+        let speed = (self.time * 1000_000.0 / num as f64) as u32;
+        if self.speed > 0.0 {
+            pulse.change_dir(0);
+        } else {
+            pulse.change_dir(1);
+        }
+        for _ in 0..num {
+            pulse.sent_pulse(delay);
+            delay.delay_us(speed);
+        }
+
+        n
+    }
+    fn variable_speed(
+        &self,
+        now_angle: f64,
+        pulse: &mut Pulse,
+        delay: &mut Delay,
+        bit_angle: f64,
+    ) -> i32 {
+        // let n =
+        //     ((self.speed * self.time + 0.5 * self.a * self.time * self.time) / bit_angle) as i32;
+
+        // let mut speed = self.speed;
+        // let num = n.abs();
+
+        // for i in 0..num {
+        //     speed = self.speed + self.a * self.time * (i as f64 / num as f64);
+
+        //     if speed > 0.0 {
+        //         pulse.change_dir(0);
+        //     } else {
+        //         pulse.change_dir(1);
+        //     }
+        //     let mut dur = (1000_000.0 / (speed / bit_angle)) as u32;
+        //     pulse.sent_pulse(delay);
+        //     delay.delay_us(dur);
+        // }
+        let subdivide = 100;
+        let mut n = 0;
+        let mut speed = self.speed;
+        let dur_time = self.time / subdivide as f64;
+        for i in 1..(subdivide + 1) {
+            speed = self.speed + self.a * dur_time * i as f64;
+            if speed > 0.0 {
+                pulse.change_dir(0);
+            } else {
+                pulse.change_dir(1);
+            }
+
+            n += self.v_uniform_speed(pulse, delay, bit_angle, dur_time, speed);
+        }
+
+        n
+    }
+    fn v_uniform_speed(
+        &self,
+        pulse: &mut Pulse,
+        delay: &mut Delay,
+        bit_angle: f64,
+        dur_time: f64,
+        now_speed: f64,
+    ) -> i32 {
+        let n = (now_speed * dur_time / bit_angle) as i32;
+        let num = n.abs();
+        let speed = (dur_time * 1000_000.0 / num as f64) as u32;
+        for _ in 0..num {
             pulse.sent_pulse(delay);
             delay.delay_us(speed);
         }
@@ -339,27 +496,30 @@ struct MoveMent {
     angle: f64,
     bit_angle: f64,
     as5600: As5600,
-    r_path: Vec<RPoint, 10>,
+    r_path: [RPoint; 10],
     capacity: usize,
     now_capacity: usize,
 }
 impl MoveMent {
     fn new(mut as5600: As5600) -> Self {
-        let now_angle = as5600.get_angle();
+        // let now_angle = as5600.get_angle();
+        let now_angle = 0.0;
+        let capacity = 10;
+
         Self {
             angle: now_angle,
             bit_angle: 0.0434363,
             as5600,
-            r_path: Vec::new(),
-            capacity: 10,
+            r_path: [RPoint::new(0.0, 0.0, 0.0, 0.0, 0); 10],
+            capacity,
             now_capacity: 0,
         }
     }
 
-    //添加路径
+    // 添加路径
     fn add_point(&mut self, point: RPoint) -> Result<(), ()> {
         if self.now_capacity < self.capacity {
-            self.r_path.push(point).unwrap();
+            self.r_path[self.now_capacity] = point;
             self.now_capacity += 1;
             Ok(())
         } else {
@@ -368,11 +528,18 @@ impl MoveMent {
     }
 
     fn clean_path(&mut self) {
-        self.r_path = Vec::new();
         self.now_capacity = 0;
     }
 
     fn start(&mut self, pulse: &mut Pulse, delay: &mut Delay) -> Result<(), ()> {
+        for i in 0..self.now_capacity {
+            let n = self.r_path[i].start(self.angle, self.bit_angle, pulse, delay);
+            self.angle += self.bit_angle * n as f64;
+            let mut buf = FmtBuf::new();
+            buf.reset();
+            write!(&mut buf, "{} {}\n", self.angle, &n).unwrap();
+            sent_message(buf);
+        }
         Ok(())
     }
 }
